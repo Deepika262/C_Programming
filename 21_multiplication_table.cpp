ab285fff8e3278068207b5a7f7e944ce7dbd6b4b
DEEPIKA M <mmdeepikabcashift@gmail.com>
2025-05-20 12:06:08 +0530

    Add files via upload

---
+++ 21_multiplication_table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    {
+         cout<<"Enter a Number";
+    cin>>n;
+
+    }
+    for(int i=1;i<=10;i++)
+    {
+        cout<<n<<"x"<<i<<"="<<i*n;
+        cout<<endl;
+
+    }
+    return 0;
+
+   
+}
+
